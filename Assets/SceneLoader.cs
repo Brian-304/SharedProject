087f1d300440c6caf1d896408e6beab6a430ed1c
Brian Tabios <105179017+Brian-304@users.noreply.github.com>
2024-12-03 01:33:41 -0800

    Modify Scenes

--- Assets/SceneLoader.cs
+++ Assets/SceneLoader.cs
@@ -6,9 +6,22 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     // Start is called before the first frame update
+
     public void LoadScene1()
     {
         // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("Scenes/Scene1");
+    }
+    public void LoadScene2()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("Scenes/Scene2");
+    }
+
+    public void LoadScene3()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        
         SceneManager.LoadScene("Scenes/TwistOfFates");
     }
 }
